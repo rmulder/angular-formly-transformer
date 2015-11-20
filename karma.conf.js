99eadb07c8b98487f4a872d120cb42678ef60c84
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-20 02:43:48 +0100

    Set karma as single run

--- karma.conf.js
+++ karma.conf.js
@@ -84,10 +84,6 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
-
-        // Concurrency level
-        // how many browser should be started simultanous
-        concurrency: Infinity
+        singleRun: true
     })
 };
